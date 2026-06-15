a1e9fdb6c7e534452c1fdb7db8e3ac03635b4b85
edu_sank <edusanki123@gmail.com>
2026-06-15 11:48:10 -0300

    Uniciclo.v atualizado

--- Uniciclo.v
+++ Uniciclo.v
@@ -1,47 +1,60 @@
 module Uniciclo (
-	input logic clockCPU, clockMem,
-	input logic reset,
-	output logic [31:0] PC,
-	output logic [31:0] Instr,
-	input  logic [4:0] regin,
-	output logic [31:0] regout
-	);
-	
-	
-	initial
-		begin
-			PC<=32'h0040_0000;
-			Instr<=32'b0;
-			regout<=32'b0;
-		end
-		
-		wire [31:0] SaidaULA, Leitura2, MemData;
-		wire EscreveMem;
-		
-//******************************************
-// Aqui vai o seu código do seu processador
+    input logic clockCPU, clockMem,
+    input logic reset,
+    output logic [31:0] PC,
+    output logic [31:0] Instr,
+    input  logic [4:0] regin,
+    output logic [31:0] regout
+);
+    
+    // DECLARAÇÃO DOS SINAIS INTERNOS
+	 
+    // Sinais de controle de 1 bit
+    logic EscreveMem;
+    logic Mem2Reg;
+
+    // Barramento de dados de 32 bits
+    logic [31:0] SaidaULA;
+    logic [31:0] ReadData1;
+    logic [31:0] ReadData2;
+    logic [31:0] ReadData;   // Este sinal vai receber a saída da Memória de Dados
+    logic [31:0] WriteData;
+    logic [31:0] SaidaImediato;
+
+
+    // ATUALIZAÇÃO DO PC
+    always @(posedge clockCPU or posedge reset) begin
+        if(reset) begin
+            PC <= 32'h0040_0000;
+        end else begin
+            PC <= PC + 4;
+        end
+    end
 
 
 
-always @(posedge clockCPU  or posedge reset)
-	if(reset)
-		PC <= 32'h0040_0000;
-	else
-		PC <= PC + 4;
+    // 1) MEMÓRIA DE INSTRUÇÕES: 1024 words
+    ramI MemC (
+        .address(PC[11:2]),
+        .clock(clockMem),
+        .data(32'b0),
+        .wren(1'b0),
+        .q(Instr)
+    );
 
-		
-		
-assign EscreveMem = 1'b0;
-assign SaidaULA = 32'b0;
+    // 2) MEMÓRIA DE DADOS: 1024 words
+    ramD MemD (
+        .address(SaidaULA[11:2]),
+        .clock(clockMem),
+        .data(ReadData2),
+        .wren(EscreveMem),
+        .q(ReadData)
+    );
+     
+     
 
-
-// Instanciação das memórias
-ramI MemC (.address(PC[11:2]), .clock(clockMem), .data(), .wren(1'b0), .q(Instr));
-ramD MemD (.address(SaidaULA[11:2]), .clock(clockMem), .data(Leitura2), .wren(EscreveMem), .q(MemData));
-		
-
-	
-		
-//*****************************************	
-			
+    // MULTIPLEXADOR ESCRITA REG (Mux do Mem2Reg)
+   
+    assign WriteData = (Mem2Reg) ? ReadData : SaidaULA;
+    
 endmodule
